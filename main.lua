91ccf8d39a9d055aa9889580417e3db476a41d32
Caro Byoute <caroline.beyne@gmail.com>
2015-02-19 00:47:39 +0100

    Check lines working, gives all lines that are filled. UpdateGrid for removing the lines not working

--- main.lua
+++ main.lua
@@ -83,6 +83,8 @@ function love.load()
       { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
   }
   
+  print("#map", #map, "#map[#map]", #map[#map])
+  
   window = {}
   window.x = 20
   window.y = 20
@@ -100,8 +102,14 @@ end
 function love.update(dt)
   timer = timer + dt
   
-  if (madeALine()) then
-    updateGrid()
+--  if (madeALine()) then
+--    updateGrid()
+--  end
+
+  local res_lines = checkForLine()
+  
+  if next(res_lines) ~= nil then
+    updateGrid(res_lines)
   end
   
   if not testMap(0,1) then
@@ -165,25 +173,26 @@ end
 -------------------------------------
 -------------------------------------
 function testMap(x, y)
-    if next(current) == nil then
-      return false
-    else
-      local nb_pieces = 0
+  if next(current) == nil then
+    return false
+  else
+    local i
+    local nb_pieces = 0
+    
+    for i = 1, #current do
+      local px = current[i][1]
+      local py = current[i][2]
       
-      for i = 1, #current do
-        local px = current[i][1]
-        local py = current[i][2]
-        
-        if ((py + y) <= #map) and ((py + y) > 0) and
-          ((px + x) <= #map[#map]) and ((px + x) > 0) then
-            if (map[py+y][px+x] ~= 1) then
-              nb_pieces = nb_pieces + 1
-            end
-        end
+      if ((py + y) <= #map) and ((py + y) > 0) and
+        ((px + x) <= #map[#map]) and ((px + x) > 0) then
+          if (map[py+y][px+x] ~= 1) then
+            nb_pieces = nb_pieces + 1
+          end
       end
-      
-      return (nb_pieces == #current)
     end
+    
+    return (nb_pieces == #current)
+  end
 end
 
 -------------------------------------
@@ -224,16 +233,15 @@ end
 
 
 function newShape()
+  local i
+  
   for i = 1, #current do
     local x = current[i][1]
     local y = current[i][2]
     map[y][x] = 1
   end
-  
-  local r = math.random(1,8)
-  print("r = ", r)
-  print("Shapes r", Shapes[r])
-  current = deepCopy(Shapes[r])
+
+  current = deepCopy(Shapes[math.random(1,8)])
   
   for i = 1, #current do
     local x = current[i][1]
@@ -241,7 +249,7 @@ function newShape()
     map[y][x] = 2
   end
   
-  debugPrintMatrix()
+--  debugPrintMatrix()
 end
 
 
@@ -251,6 +259,8 @@ end
 -------------------------------------
 -------------------------------------
 function updateMapDown()
+  local i 
+  
   for i = 1, #current  do
     local x = current[i][1]
     local y = current[i][2]
@@ -269,6 +279,8 @@ end
 -------------------------------------
 -------------------------------------
 function updateMapLeft()
+  local i
+  
   for i = 1, #current do
     local x = current[i][1]
     local y = current[i][2]
@@ -289,6 +301,8 @@ end
 -------------------------------------
 -------------------------------------
 function updateMapRight()
+  local i
+  
   for i = 1, #current do
     local x = current[i][1]
     local y = current[i][2]
@@ -331,17 +345,29 @@ end
 
 -------------------------------------
 -------------------------------------
--- madeALine = IF A LINE IS COMPLETE RETURNS YES
+-- checkForLine = IF A LINE IS COMPLETE RETURNS YES
 -------------------------------------
 -------------------------------------
-function madeALine()
-  local i
-  for i=1,#map,1 do
-    if map[#map][i] == 0 then
-      return false
+function checkForLine()
+  local h, w
+  local nb_pixels = 0
+  local res_lines = {}
+  
+  for h=1,#map do
+    nb_pixels = 0
+    
+    for w=1,#map[#map] do
+      if map[h][w] == 1 then
+        nb_pixels = nb_pixels + 1
+      end
+    end
+    
+    if nb_pixels == #map[#map] then
+      table.insert(res_lines, h)
     end
   end
-  return true
+ 
+  return res_lines
 end
 
 -------------------------------------
@@ -349,23 +375,38 @@ end
 -- updateGrid = IF A LINE IS MADE, ERASES IT AND STACKS EVERYTHING ONE LEVEL DOWN & MAKES A NEW SQUARE
 -------------------------------------
 -------------------------------------
-function updateGrid()
-  local h, w
+function updateGrid(res_lines)
+  for k, v in pairs(res_lines) do
+    local line = v
+    local h, w
     
-  for w=1,#map[#map],1 do
-    map[#map][w] = 0
-  end
-  
-  local point
-  
-  for h=#map-1,1,-1 do
-    for w=#map[h],1,-1 do
-      if (map[h][w] == 1) then
-        map[h][w] = 0
-        map[h+1][w] = 1
-      end
+    for w=1,#map[#map] do
+      map[line][w] = 0
     end
+    
+    for h=line,1,-1 do
+      for w=1,#map[#map] do
+        map[h][w] = map[h-1][w]
+      end
+    end    
   end
+  
+--  local h, w
+    
+--  for w=1,#map[#map],1 do
+--    map[#map][w] = 0
+--  end
+  
+--  local point
+  
+--  for h=#map-1,1,-1 do
+--    for w=#map[h],1,-1 do
+--      if (map[h][w] == 1) then
+--        map[h][w] = 0
+--        map[h+1][w] = 1
+--      end
+--    end
+--  end
 
   newShape()
 end
@@ -388,7 +429,6 @@ function debugPrintMatrix()
   end
   print("\n\n")
 end
-
 
 
 function deepCopy(original)
